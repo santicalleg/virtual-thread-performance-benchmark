0e2eca86c42bd0f626e7a862d69b644b7dcd7096
Santiago Calle Gomez <sancalle@sancalle.local>
2023-06-29 14:40:51 -0500

    fix: python update implementation to graphql

--- python/application/main.py
+++ python/application/main.py
@@ -1,4 +1,4 @@
-from infrastructure.entry_point.api import app as api
+from infrastructure.entry_point.graphql import app as api
 # from infrastructure.entry_point.graphql import app as api
 
 app = api.create_app()
